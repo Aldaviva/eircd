a0b9a0dd7bd00346b54be5ce5121a4fee6bd0a4c
Ben Hutchison <ben@aldaviva.com>
2012-12-01 05:16:50 -0800

    Previous commit missed a renamed file.

---
+++ eircd.erl
@@ -0,0 +1,30 @@
+-module(eircd).
+-export([start/0, start/1]).
+-include("constants.hrl").
+
+start() ->
+	start(6667).
+start(Port) ->
+	create_users_table(),
+	create_channels_table(),
+	start_listener(Port),
+	ok.
+
+create_users_table() ->
+	ets:new(users, [
+		public,
+		named_table,
+		{keypos, 2},
+		{read_concurrency, true}
+	]).
+
+create_channels_table() ->
+	ets:new(channels, [
+		public,
+		named_table,
+		{keypos, 2},
+		{read_concurrency, true}
+	]).
+
+start_listener(Port) ->
+	spawn_link(listener, listen, [Port]).
